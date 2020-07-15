2100b72f4901d754f9df99702618c4146821e4de
slevinger <shir.levinger@gmail.com>
2020-07-15 17:30:51 +0300

    remove redundent actions

--- src/context/actionTypes/vastsActions.js
+++ src/context/actionTypes/vastsActions.js
@@ -3,10 +3,3 @@ export const ADD_VAST = "ADD_VAST";
 export const UPDATE_VAST = "UPDATE_VAST";
 export const SET_VASTS = "SET_VASTS";
 export const SET_ERROR = "SET_ERROR";
-
-export const SET = "SET";
-export const SET_ID = "SET_ID";
-export const SET_URL = "SET_URL";
-export const SET_WIDTH = "SET_WIDTH";
-export const SET_HEIGHT = "SET_HEIGHT";
-export const SET_POSITION = "SET_POSITION";
